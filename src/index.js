3b0b22716bbd902af89310a21f5a3925c2e99ec0
Donovan Weber <donovanweber03@gmail.com>
2022-06-28 14:39:42 -0400

    Separates promise logic from search and random
    Co-authored-by: Tyler Emmerson <tyleremmerson0216@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,30 +2,72 @@ import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import GiphyService from './giphy.service.js';
+import GiphyRandom from './giphy-random-service.js';
 
 $(document).ready(function () {
   $("form#gif").submit(function (event) {
     event.preventDefault();
     const userSearch = $("#search").val();
     $("#search").val();
+    
+    let promise = GiphyService.getGif(userSearch);
 
+      promise.then((response) => {
+        const body = JSON.parse(response);
+      $("#output-image1").html(
+        `<img src="${body.data[0].images.original.url}"></img>`
+      );
+      $("#output-image2").html(
+        `<img src="${body.data[1].images.original.url}"></img>`
+      );
+      $("#output-image3").html(
+        `<img src="${body.data[2].images.original.url}"></img>`
+      );
+    })
+  });
+
+  $("button#trending").click(function (event) {
+    event.preventDefault();
+  let promise = new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${userSearch}&limit=1&offset=0&rating=g&lang=en`;
+    const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=3&rating=g`;
 
-    request.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        console.log(response);
-        getElements(response);
+    request.onload = function () {
+      if (this.status === 200) {
+        resolve(request.response);
+      } else {
+        reject(request.response);
       }
-    };
-
+    }
     request.open("GET", url, true);
     request.send();
+  });
 
-       function getElements(response) {
-        console.log(response);
-      $("#output-image").html(`<img src="${response.data[0].images.original.url}"></img>`);
-  }
-});
+  
+    function outputTrending(response) {
+      const body = JSON.parse(response);
+      $("#output-image1").html(
+        `<img src="${body.data[0].images.original.url}"></img>`
+      );
+      $("#output-image2").html(
+        `<img src="${body.data[1].images.original.url}"></img>`
+      );
+      $("#output-image3").html(
+        `<img src="${body.data[2].images.original.url}"></img>`
+      );
+    }
+  });
+  $("button#random").click(function (event) {
+    event.preventDefault();
+    let promise = GiphyRandom.getGif();
+
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $("#output-image1").html(
+        `<img src="${body.data.images.original.url}"></img>`);
+      $("#output-image2").empty();
+      $("#output-image3").empty();
+    })
+  });
 });
